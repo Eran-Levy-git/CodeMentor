a4b9464f585d96c696cfc69808a1c589c1576182
Eran <eran.levi3@mail.huji.ac.il>
2023-07-24 16:10:02 +0300

    easy solution for code block 1 smiley is showing 	modified:   app.js 	modified:   views/code-block.ejs

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const io = new Server(server);
 
 // Sample code block data (replace this with your actual data)
 const codeBlocks = [
-    { id: 1, title: 'Async case', code: 'async function fetchData() {\n  // Async code here\n}', solution: 'async function fetchData() {\n  return fetch("https://api.example.com/data");\n}' },
+    { id: 1, title: 'Async case', code: 'async function fetchData() {\n  // Async code here\n}', solution: 'E=MC^2' },
     { id: 2, title: 'Promises', code: 'function fetchData() {\n  return new Promise((resolve, reject) => {\n    // Promise code here\n  });\n}', solution: 'function fetchData() {\n  return new Promise((resolve, reject) => {\n    resolve("Data loaded successfully");\n  });\n}' },
     { id: 3, title: 'Callbacks', code: 'function fetchData(callback) {\n  // Callback code here\n}', solution: 'function fetchData(callback) {\n  setTimeout(() => {\n    callback("Data loaded successfully");\n  }, 2000);\n}' },
     { id: 4, title: 'Event handling', code: 'document.addEventListener("click", (event) => {\n  // Event handling code here\n});', solution: 'document.addEventListener("click", (event) => {\n  console.log("Clicked!");\n});' }
