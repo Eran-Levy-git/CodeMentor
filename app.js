11af16cb382fb49ba28ed50e278f798e9eb4e75a
Eran <eran.levi3@mail.huji.ac.il>
2023-07-25 13:54:54 +0300

    modified:   app.js

--- app.js
+++ app.js
@@ -41,3 +41,10 @@ io.on('connection', (socket) => {
     });
 });
 
+// Use PORT provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
+  // ...
+});
