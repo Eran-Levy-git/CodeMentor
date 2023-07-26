99c6782e828c6d1d7a4c5bc125c076d242a56920
Eran <eran.levi3@mail.huji.ac.il>
2023-07-26 12:32:58 +0300

    fin Changes to be committed: 	modified:   app.js 	modified:   views/code-block.ejs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const WebSocket = require('ws');
 
 // Sample code block data (replace this with your actual data)
 const codeBlocks = [
-    { id: 1, title: 'Async case', code: 'async function fetchData() {\n  // Async code here\n}', solution: 'E=MC^2' },
+    { id: 1, title: 'Async case', code: 'async function fetchData() {\n  // Async code here\n}', solution: 'e=mc^2' },
     { id: 2, title: 'Promises', code: 'function fetchData() {\n  return new Promise((resolve, reject) => {\n    // Promise code here\n  });\n}', solution: 'function fetchData() {\n  return new Promise((resolve, reject) => {\n    resolve("Data loaded successfully");\n  });\n}' },
     { id: 3, title: 'Callbacks', code: 'function fetchData(callback) {\n  // Callback code here\n}', solution: 'function fetchData(callback) {\n  setTimeout(() => {\n    callback("Data loaded successfully");\n  }, 2000);\n}' },
     { id: 4, title: 'Event handling', code: 'document.addEventListener("click", (event) => {\n  // Event handling code here\n});', solution: 'document.addEventListener("click", (event) => {\n  console.log("Clicked!");\n});' }
